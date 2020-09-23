4a3854c78454a17614c0314f8f497e024945539a
Kamil Grabek <kamil.grabek@allegro.pl>
2020-09-23 21:24:20 +0200

    sample response

--- server.ts
+++ server.ts
@@ -2,8 +2,27 @@ const { Client } = require('pg')
 const client = new Client()
 
 const tasksQuery = `
-  SELECT t.title, t.info, s.value statusValue, s.label statusLabel, p.value priorityValue, p.label priorityLabel FROM tasks6 t LEFT JOIN statuses s ON s.id = t.statusId INNER JOIN priorities p ON p.id = t.priorityId;
+  SELECT t.title, t.info, s.value status_value, s.label status_label, p.value priority_value, p.label priority_label FROM tasks6 t LEFT JOIN statuses s ON s.id = t.statusId INNER JOIN priorities p ON p.id = t.priorityId;
 `
+// Sample response
+// [
+//   {
+//     title: 'Wypełnić bazę',
+//     info: 'Jakies pierdoly 2',
+//     status_value: 'TODO      ',
+//     status_label: 'To do',
+//     priority_value: 'YELLOW    ',
+//     priority_label: 'Ważne'
+//   },
+//   {
+//     title: 'Nauczyć sie postsql',
+//     info: 'brak',
+//     status_value: 'TODO      ',
+//     status_label: 'To do',
+//     priority_value: 'YELLOW    ',
+//     priority_label: 'Ważne'
+//   }
+// ]
 
 const express = require('express')
 const app = express()
