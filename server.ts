e8792602535621b5765212763287d8e14b278d9e
Kamil Grabek <kamil.grabek@allegro.pl>
2020-09-23 21:19:51 +0200

    express server

--- server.ts
+++ server.ts
@@ -1,13 +1,23 @@
 const { Client } = require('pg')
 const client = new Client()
 
-async function start() {
+const tasksQuery = `
+  SELECT t.title, t.info, s.value statusValue, s.label statusLabel, p.value priorityValue, p.label priorityLabel FROM tasks6 t LEFT JOIN statuses s ON s.id = t.statusId INNER JOIN priorities p ON p.id = t.priorityId;
+`
+
+const express = require('express')
+const app = express()
+
+app.get('/', async (req: any, res: any) => {
+  res.send(await runQuery())
+})
+
+app.listen(3000)
+
+async function runQuery() {
   await client.connect()
-  const res = await client.query('SELECT * FROM tasks1;')
+  const res = await client.query(tasksQuery)
   console.log(res.rows)
   await client.end()
+  return res.rows;
 }
-
-start().then(() => {
-  console.log('###########');
-});
